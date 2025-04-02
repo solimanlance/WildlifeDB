fef4c6243d852d74c87446f9dc80dbfe1a566074
Bryan <b.mpark0722@gmail.com>
2025-04-02 16:35:28 -0700

    fix

--- gui/public/scripts.js
+++ gui/public/scripts.js
@@ -11,22 +11,17 @@
  *   HTML structure.
  * 
  */
-
-
 // This function checks the database connection and updates its status on the frontend.
 async function checkDbConnection() {
     const statusElem = document.getElementById('dbStatus');
     const loadingGifElem = document.getElementById('loadingGif');
-
     const response = await fetch('/check-db-connection', {
         method: "GET"
     });
-
     // Hide the loading GIF once the response is received.
     loadingGifElem.style.display = 'none';
     // Display the statusElem's text in the placeholder.
     statusElem.style.display = 'inline';
-
     response.text()
     .then((text) => {
         statusElem.textContent = text;
@@ -35,24 +30,19 @@ async function checkDbConnection() {
         statusElem.textContent = 'connection timed out';  // Adjust error handling if required.
     });
 }
-
 // Fetches data from the demotable and displays it.
 async function fetchAndDisplayUsers() {
     const tableElement = document.getElementById('demotable');
     const tableBody = tableElement.querySelector('tbody');
-
     const response = await fetch('/demotable', {
         method: 'GET'
     });
-
     const responseData = await response.json();
     const demotableContent = responseData.data;
-
     // Always clear old, already fetched data before new fetching process.
     if (tableBody) {
         tableBody.innerHTML = '';
     }
-
     demotableContent.forEach(user => {
         const row = tableBody.insertRow();
         user.forEach((field, index) => {
@@ -61,14 +51,12 @@ async function fetchAndDisplayUsers() {
         });
     });
 }
-
 // This function resets or initializes the demotable.
 async function resetDemotable() {
     const response = await fetch("/initiate-demotable", {
         method: 'POST'
     });
     const responseData = await response.json();
-
     if (responseData.success) {
         const messageElement = document.getElementById('resetResultMsg');
         messageElement.textContent = "demotable initiated successfully!";
@@ -77,66 +65,74 @@ async function resetDemotable() {
         alert("Error initiating table!");
     }
 }
-
-
-// Updates names in the demotable.
+// Inserts new records into the demotable.
 async function insertDemotable(event) {
     event.preventDefault();
-
-    const animalId = parseInt(document.getElementById('insertId').value);
-    const habitatId = parseInt(document.getElementById('insertHabitat').value);
-    const researchTeamId = parseInt(document.getElementById('insertTeam').value);
-
-    const speciesName = document.getElementById('insertSpecies').value;
-
-
+    const idValue = document.getElementById('insertId').value;
+    const nameValue = document.getElementById('insertName').value;
     const response = await fetch('/insert-demotable', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            animal_id: animalId,
-            habitat_id: habitatId,
-            species_name: speciesName,
-            research_team_id: researchTeamId
+            id: idValue,
+            name: nameValue
         })
     });
-
     const responseData = await response.json();
     const messageElement = document.getElementById('insertResultMsg');
-
     if (responseData.success) {
-        messageElement.textContent = "Animal inserted successfully!";
+        messageElement.textContent = "Data inserted successfully!";
         fetchTableData();
     } else {
-        messageElement.textContent = "Error inserting animal!";
+        messageElement.textContent = "Error inserting data!";
     }
 }
-
-
+// Updates names in the demotable.
+async function updateNameDemotable(event) {
+    event.preventDefault();
+    const oldNameValue = document.getElementById('updateOldName').value;
+    const newNameValue = document.getElementById('updateNewName').value;
+    const response = await fetch('/update-name-demotable', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            oldName: oldNameValue,
+            newName: newNameValue
+        })
+    });
+    const responseData = await response.json();
+    const messageElement = document.getElementById('updateNameResultMsg');
+    if (responseData.success) {
+        messageElement.textContent = "Name updated successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error updating name!";
+    }
+}
 // Counts rows in the demotable.
 // Modify the function accordingly if using different aggregate functions or procedures.
 async function countDemotable() {
     const response = await fetch("/count-demotable", {
         method: 'GET'
     });
-
     const responseData = await response.json();
     const messageElement = document.getElementById('countResultMsg');
-
     if (responseData.success) {
         const tupleCount = responseData.count;
         messageElement.textContent = `The number of tuples in demotable: ${tupleCount}`;
     } else {
-        messageElement.textContent = "Error counting tuples!";
+        alert("Error in count demotable!");
     }
 }
 
 async function groupByQueryFunctionality() {
     try {
         const response = await fetch('/group-by-query');
-        
+
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -168,7 +164,6 @@ async function groupByQueryFunctionality() {
         document.getElementById('groupByResult').textContent = 'Error fetching the data from the server.';
     }
 }
-
 
 // ---------------------------------------------------------------
 // Initializes the webpage functionalities.
